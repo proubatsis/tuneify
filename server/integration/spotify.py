e43281a11a17df90bbbf60d5935a3b2a79ae737c
viktree <vikram.venkataramanan@mail.utoronto.ca>
2017-01-21 18:38:52 -0500

    Can now create playlists.

--- server/integration/spotify.py
+++ server/integration/spotify.py
@@ -2,6 +2,9 @@ from api.reddit import get_posts
 from integration import get_filtered_posts
 from api.spotify import findSong
 
+import spotipy
+import spotipy.util as util
+
 
 def get_tracks(subreddit):
     """
@@ -25,3 +28,12 @@ def get_tracks(subreddit):
     ]
 
     return tracks
+
+
+def createPlaylist(token, user, subreddit, playlist_name):
+    sp = spotipy.Spotify(auth=token)
+    playlist = sp.user_playlist_create(user, playlist_name)
+    tracks = get_tracks(subreddit)
+    tracks = [t["uri"] for t in tracks]
+    sp.user_playlist_add_tracks(user, playlist['id'], tracks)
+
