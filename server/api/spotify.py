48221c64266ce5de889d7d4e8e812bec0d4dde82
viktree <vikram.venkataramanan@mail.utoronto.ca>
2017-01-21 13:31:15 -0500

    Can search on Spotify

---
+++ server/api/spotify.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+###############################################################################
+#     File Name           :     spotify.py
+#     Created By          :     vikram
+#     Creation Date       :     [2017-01-21 00:57]
+#     Last Modified       :     [2017-01-21 13:28]
+#     Description         :     Interacts with the python file
+###############################################################################
+
+# import sys
+import spotipy
+
+name = "Drake"
+
+
+def findSong(name):
+    """TODO: Docstring for findSong.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    spotify = spotipy.Spotify()
+    results = spotify.search(q='track:' + name, type='track')
+
+    items = results['tracks']['items']
+
+    print(items[0])
+
+
+def findArtist(arg1):
+    """TODO: Docstring for findArtist.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    spotify = spotipy.Spotify()
+    results = spotify.search(q='artist:' + name, type='artist')
+    items = results['artists']['items']
+    if len(items) > 0:
+        artist = items[0]
+        print(artist['name'], artist['images'][0]['url'])
+
+
+if __name__ == "__main__":
+    findSong("Jimi Hendrix - Purple Haze")
